28a30bef186975d5dfd6e2d46f5dd951030715d3
SamiBenAmara <samibenamara23@gmail.com>
2020-02-05 23:56:05 -0800

    DCP88.java

---
+++ DCP88.java
@@ -0,0 +1,24 @@
+import java.util.Scanner.*;
+import java.util.*;
+
+class DCP88
+{
+	public static int div(int x, int y)
+	{
+		if (x - y < 0) return 0;
+		return 1 + div(x - y, y);
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter a number: ");
+		int a = scan.nextInt();
+
+		System.out.println("Enter another number: ");
+		int b = scan.nextInt();
+
+		System.out.println(div(a, b));
+	}
+}
