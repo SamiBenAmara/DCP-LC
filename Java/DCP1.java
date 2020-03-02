b1795fce55c9a2eedef8def813ce8bc3a51f501e
SamiBenAmara <https://github.com/SamiBenAmara/Data-Structures-Visualizer.git>
2020-03-01 23:56:37 -0800

    Commented DCP1

--- Java/DCP1.java
+++ Java/DCP1.java
@@ -5,11 +5,14 @@ class DCP1
 	public static boolean twoSum(int[] arr, int target) 
     {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        
+     	
+     	// Add the values of the array to a hashmap, using the array value as the key   
         for (int i = 0; i < arr.length; i++) map.put(arr[i], i);
         
+        // Iterate through the array
         for (int i = 0; i < arr.length; i++)
         {
+        	// Check whether the hashmap contains a value that is equal to the target value minus an array value 
             int t = target - arr[i];
             if (map.containsKey(t) && map.get(t) != i) return true;
         }
